dea61bd6ae267c616d670432cb850010431c3ff9
sumathi-4 <sumathisaraswathi4@gmail.com>
2026-06-05 18:01:10 +0530

    change only showing admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,71 +1,7 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
-
-import Home from "./pages/Home";
-
-import Countries from "./pages/Countries";
-
-import ApplyVisa from "./pages/ApplyVisa";
-
-import TrackVisa from "./pages/TrackVisa";
-
-import About from "./pages/About";
-
-import Contact from "./pages/Contact";
 import AdminDashboard from "./pages/AdminDashboard";
 
-import Loader from "./components/Loader/Loader";
-import ScrollProgress from "./components/ScrollProgress/ScrollProgress";
-import Login from "./pages/Auth/Login";
-import Register from "./pages/Auth/Register";
 function App() {
-
-  const [loading,setLoading] = useState(true);
-
-  useEffect(()=>{
-
-    setTimeout(()=>{
-
-      setLoading(false);
-
-    },2500);
-
-  },[]);
-
-  if(loading){
-
-    return <Loader />;
-  }
-
-  return (
-
-    <>
-    
-      <ScrollProgress />
-
-      <Routes>
-
-        <Route path="/" element={<Home />} />
-
-        <Route path="/countries" element={<Countries />} />
-
-        <Route path="/apply-visa" element={<ApplyVisa />} />
-
-        <Route path="/track-visa" element={<TrackVisa />} />
-
-        <Route path="/about" element={<About />} />
-
-        <Route path="/contact" element={<Contact />} />
-        
-       <Route path="/visa-control-center" element={<AdminDashboard />} />
-        <Route path="/login" element={<Login />} />
-
-<Route path="/register" element={<Register />} />
-      </Routes>
-
-    </>
-
-  );
+  return <AdminDashboard />;
 }
 
 export default App;
